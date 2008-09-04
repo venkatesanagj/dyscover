e38b4d9875611fd123e6c039f90e2d80b0121d7c
piyubharati@gmail.com <piyubharati@gmail.com@26b7b831-a152-0410-962d-43de81d72f3e>
2008-09-04 10:32:35 +0000

    This is TTS module

---
+++ trunk/TTS/src/tts/Central.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tts;
+
+import javax.sound.midi.Synthesizer;
+
+/**
+ *
+ * @author karekar
+ */
+class Central {
+
+    static Synthesizer createSynthesizer(SynthesizerModeDesc synthesizerModeDesc) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
---
+++ trunk/TTS/src/tts/synth.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tts;
+
+/**
+ *
+ * @author karekar
+ */
+class synth {
+
+    static void deallocate() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
