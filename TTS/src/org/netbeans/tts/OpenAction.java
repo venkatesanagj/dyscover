e76e7e19bb0f79614b85cef0a22a7a6586b5faab
piyubharati <piyubharati@26b7b831-a152-0410-962d-43de81d72f3e>
2008-10-21 08:37:30 +0000

    This is the actual Text 2 Speech module suite which is created with the window component.

--- TTS/src/org/netbeans/tts/OpenAction.java
+++ TTS/src/org/netbeans/tts/OpenAction.java
@@ -4,6 +4,10 @@
  */
 package org.netbeans.tts;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import javax.swing.JFileChooser;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.CallableSystemAction;
@@ -11,6 +15,42 @@ import org.openide.util.actions.CallableSystemAction;
 public final class OpenAction extends CallableSystemAction {
 
     public void performAction() {
+         OpeningPageTopComponent op = new OpeningPageTopComponent();
+         JFileChooser fd = new JFileChooser();
+    String data = "";
+    String data1 = "";
+    int result = fd.showDialog(null, "Select any document");
+    File selectedFile;
+    if (result == JFileChooser.APPROVE_OPTION) {
+        selectedFile = fd.getSelectedFile(); // where this is a class field
+        String fileName = selectedFile.getName();
+        String filePath = selectedFile.getPath();
+        try {
+            // Open the file that is the first
+            // command line parameter
+            FileInputStream fstream = new FileInputStream(filePath);
+
+            // Convert our input stream to a
+            // DataInputStream
+            DataInputStream in =
+                    new DataInputStream(fstream);
+
+            // Continue to read lines while
+            // there are still some left to read
+            while (in.available() != 0) {
+                // Print file line to screen
+                data = (in.readLine());
+                data1 = data1 + data;
+                textArea.setText(data1);
+            }
+
+            in.close();
+        } catch (Exception e) {
+            System.err.println("File input error");
+        }
+
+
+    }
         // TODO implement action body
     }
 
--- TTS/src/org/netbeans/tts/UndoAction.java
+++ TTS/src/org/netbeans/tts/UndoAction.java
@@ -11,6 +11,7 @@ import org.openide.util.actions.CallableSystemAction;
 public final class UndoAction extends CallableSystemAction {
 
     public void performAction() {
+         OpeningPageTopComponent op = new OpeningPageTopComponent();
         // TODO implement action body
     }
 
