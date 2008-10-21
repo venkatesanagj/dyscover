4174e54ec0ed811704dd6d83d4eea40708b5c28d
piyubharati <piyubharati@26b7b831-a152-0410-962d-43de81d72f3e>
2008-10-21 06:36:24 +0000

    This is the actual Text 2 Speech module suite which is created with the window component.

--- TTS/src/org/netbeans/tts/OpeningPageTopComponent.java
+++ TTS/src/org/netbeans/tts/OpeningPageTopComponent.java
@@ -1,4 +1,5 @@
 package org.netbeans.tts;
+
 import java.io.Serializable;
 import org.openide.util.NbBundle;
 import org.openide.windows.TopComponent;
@@ -7,10 +8,12 @@ import com.sun.speech.freetts.VoiceManager;
 import java.util.logging.Logger;
 import javax.swing.undo.UndoManager;
 import com.sun.speech.freetts.Voice;
+
 /**
  * Top component which displays something.
  */
 public class OpeningPageTopComponent extends TopComponent {
+
     private String Word;
     private Object[] arr;
     private Object cmd;
@@ -23,16 +26,15 @@ public class OpeningPageTopComponent extends TopComponent {
     private static OpeningPageTopComponent instance;
     /** path to the icon used by the component and its open action */
 //    static final String ICON_PATH = "SET/PATH/TO/ICON/HERE";
-
     private static final String PREFERRED_ID = java.util.ResourceBundle.getBundle("org/netbeans/tts/Bundle").getString("OpeningPageTopComponent");
 
-private OpeningPageTopComponent() {
+    private OpeningPageTopComponent() {
         initComponents();
-        
+
         setName(NbBundle.getMessage(OpeningPageTopComponent.class, "CTL_OpeningPageTopComponent"));
- setToolTipText(NbBundle.getMessage(OpeningPageTopComponent.class, "HINT_OpeningPageTopComponent"));
+        setToolTipText(NbBundle.getMessage(OpeningPageTopComponent.class, "HINT_OpeningPageTopComponent"));
 //        setIcon(Utilities.loadImage(ICON_PATH, true));
-   }
+    }
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -42,6 +44,7 @@ private OpeningPageTopComponent() {
         clearB = new javax.swing.JButton();
         synB = new javax.swing.JButton();
         synBox = new javax.swing.JComboBox();
+        jButton1 = new javax.swing.JButton();
 
         textArea.setBackground(new java.awt.Color(29, 150, 150));
         textArea.setColumns(20);
@@ -64,6 +67,13 @@ private OpeningPageTopComponent() {
 
         org.openide.awt.Mnemonics.setLocalizedText(synB, org.openide.util.NbBundle.getMessage(OpeningPageTopComponent.class, "BUTTON_SYN")); // NOI18N
 
+        org.openide.awt.Mnemonics.setLocalizedText(jButton1, org.openide.util.NbBundle.getMessage(OpeningPageTopComponent.class, "OpeningPageTopComponent.jButton1.text")); // NOI18N
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -71,14 +81,19 @@ private OpeningPageTopComponent() {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(32, 32, 32)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(playB)
-                    .addComponent(clearB)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(synB)
-                        .addGap(18, 18, 18)
-                        .addComponent(synBox, 0, 134, Short.MAX_VALUE)))
+                        .addGap(32, 32, 32)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(playB)
+                            .addComponent(clearB)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(synB)
+                                .addGap(18, 18, 18)
+                                .addComponent(synBox, 0, 134, Short.MAX_VALUE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(79, 79, 79)
+                        .addComponent(jButton1)))
                 .addContainerGap())
         );
 
@@ -99,7 +114,9 @@ private OpeningPageTopComponent() {
                         .addGap(69, 69, 69)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(synB)
-                            .addComponent(synBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(synBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
+                        .addComponent(jButton1)))
                 .addContainerGap())
         );
 
@@ -108,48 +125,50 @@ private OpeningPageTopComponent() {
     }// </editor-fold>//GEN-END:initComponents
 
 private void playBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_playBActionPerformed
-String s1=playB.getText();
-      if(s1.equals("PLAY"))
-      {
-          playB.setText("STOP");
-              Thread speakThread = new Thread(new Runnable() {
+    String s1 = playB.getText();
+    if (s1.equals("PLAY")) {
+        playB.setText("STOP");
+        Thread speakThread = new Thread(new Runnable() {
 
             public void run() {
-                 speek();
+                speek();
             }
 
-                private void speek() {
-                   str = new String(textArea.getText());
-               VoiceManager voiceManager = VoiceManager.getInstance();
-               helloVoice = voiceManager.getVoice("kevin16");
-        helloVoice.allocate();
-        helloVoice.speak(str);
-        helloVoice.deallocate();
-                }
+            private void speek() {
+                str = new String(textArea.getText());
+                VoiceManager voiceManager = VoiceManager.getInstance();
+                helloVoice = voiceManager.getVoice("kevin16");
+                helloVoice.allocate();
+                helloVoice.speak(str);
+                helloVoice.deallocate();
+            }
         });
         speakThread.start();
-      }
-      else if(s1.equals("STOP"))
-      {
-          playB.setText("PLAY");
-          stop();
-      }// TODO add your handling code here:
+    } else if (s1.equals("STOP")) {
+        playB.setText("PLAY");
+        stop();
+    }// TODO add your handling code here:
 }//GEN-LAST:event_playBActionPerformed
 
 private void clearBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearBActionPerformed
-textArea.setText("");
+    textArea.setText("");
     synBox.removeAllItems();// TODO add your handling code here:
 }//GEN-LAST:event_clearBActionPerformed
 
+private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+// new PronunciationEditorTopComponent().setVisible(true);
+}//GEN-LAST:event_jButton1ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton clearB;
+    private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton playB;
     private javax.swing.JButton synB;
     private javax.swing.JComboBox synBox;
     private javax.swing.JTextArea textArea;
     // End of variables declaration//GEN-END:variables
+
     /**
      * Gets default instance. Do not use directly: reserved for *.settings files only,
      * i.e. deserialization routines; otherwise you could get a non-deserialized instance.
